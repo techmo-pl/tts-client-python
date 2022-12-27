69f3a90d003a82f5d3351cf19332d691ad3103e4
Jan Wozniak <noreply@techmo.pl>
2022-12-27 11:38:41 +0100

    rename .env --> .venv

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-env_dir=.env
+venv_dir=.venv
 
-python3 -m venv "${env_dir}"
-source "${env_dir}"/bin/activate
+python3 -m venv "${venv_dir}"
+source "${venv_dir}"/bin/activate
 pip install -e .
